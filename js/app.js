844cf38db80a30cc4a506c8d2e86135b6f96350f
John <johnngo@Johns-MBP.hitronhub.home>
2017-01-15 15:42:30 -0800

    remove codes

--- js/app.js
+++ js/app.js
@@ -132,52 +132,6 @@ Player.prototype.handleInput = function(keyPress) {
    document.body.insertBefore(scoreLevelDiv, firstCanvasTag[0]);
 };
 
-/*var checkCollision = function(anEnemy){
-    console.log(this);
-    debugger;
-    //check for collision between enemy and player
-    if (
-        player.y + 131 >= anEnemy.y + 90
-        && player.x + 25 <= anEnemy.x + 88
-        && player.y + 73 <= anEnemy.y + 135
-        && player.x + 76 >= anEnemy.x + 11) {
-        console.log('collided');
-        player.x = 202.5;
-        player.y = 383;
-}
-
-// check for player reaching top of canvas and winning the game
-// if player wins, add 1 to the score and level
-// pass score as an argument to the inccreaseDifficulty function
-    if (player.y + 63 <=0){
-        player.x = 202.5;
-        player.y = 383;
-        console.log('you made it');
-
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0, 0, 505, 171);
-
-        score += 1;
-        gameLevel +=1;
-        console.log('current score: ' + score + ', current level: ' + gameLevel);
-        increaseDifficulty(score);
-
-    }
-
-    // check if player runs into left, bottom or right canvas walls
-    //prevent player from moving beyond canvas wall boundaries
-    if (player.y > 383 ) {
-        player.y = 383;
-    }
-    if (player.x > 402.5){
-        player.x = 402.5;
-    }
-    if (player.x < 2.5){
-        player.x = 2.5;
-    }
-};
-*/
-
 // Increase number of enemies on screen based on player's score
 var increaseDifficulty = function(numEnemies) {
     // remove all previous enemies on canvas
